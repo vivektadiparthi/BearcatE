1a20e5a0b5c9e1dde319bfd1a2b5254de664e279
Vivek Tadiparthi <83790881+vivektadiparthi@users.noreply.github.com>
2021-10-25 08:43:02 -0500

    adding project to repo

---
+++ BearCat-E/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BearCat_E.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
